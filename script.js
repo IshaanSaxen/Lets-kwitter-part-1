a2ad0e3314cf52aa769c3a3d7b84c3687ddd3082
IshaanSaxen <78091439+IshaanSaxen@users.noreply.github.com>
2021-07-18 23:25:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.querySelector('.img-btn').addEventListener('click', function()
+	{
+		document.querySelector('.cont').classList.toggle('s-signup')
+	}
+);
+
+function submit(){
+
+	window.location.replace("kwitter_room.html");
+}
+
+function submit2(){
+	window.location.replace("kwitter_room.html");
+}
+
